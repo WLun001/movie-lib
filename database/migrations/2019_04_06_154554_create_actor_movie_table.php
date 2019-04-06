7f6938908df2af892a135851d96b306992cae2d0
Wei Lun <weilun_95@hotmail.com>
2019-04-06 23:54:03 +0800

    feat: migrated database

---
+++ database/migrations/2019_04_06_154554_create_actor_movie_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateActorMovieTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('actor_movie', function (Blueprint $table) {
+            $table->unsignedBigInteger('actor_id');
+            $table->unsignedBigInteger('movie_id');
+            $table->primary('actor_id', 'movie_id');
+            $table->foreign('actor_id')->references('id')->on('actors');
+            $table->foreign('movie_id')->references('id')->on('movies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('actor_movie');
+    }
+}
