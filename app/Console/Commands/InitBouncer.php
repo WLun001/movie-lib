f5795c5eec0e84404e87e57294c1297489cd3eae
Wei Lun <weilun_95@hotmail.com>
2019-04-07 14:03:19 +0800

    feat: added bouncer

---
+++ app/Console/Commands/InitBouncer.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class InitBouncer extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'init:bouncer';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Initialise roles and abilities';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        // Define roles
+        $admin = Bouncer::role()->create([
+            'name' => 'admin',
+            'title' => 'Administrator',
+        ]);
+
+        $staff = Bouncer::role()->create([
+            'name' => 'staff',
+            'title' => 'Staff',
+        ]);
+
+        $member = Bouncer::role()->create([
+            'name' => 'member',
+            'title' => 'Member',
+        ]);
+
+        // Define abilities
+        $manageUsers = Bouncer::ability()->create([
+            'name' => 'manage-users',
+            'title' => 'Manager Users',
+        ]);
+
+        $manageBooks = Bouncer::ability()->create([
+            'name' => 'manage-books',
+            'title' => 'Manager Books',
+        ]);
+
+        $viewBooks = Bouncer::ability()->create([
+            'name' => 'view-books',
+            'title' => 'View Books',
+        ]);
+
+        // Assign abilities to roles
+        Bouncer::allow($admin)->to($manageUsers);
+        Bouncer::allow($admin)->to($manageBooks);
+        Bouncer::allow($admin)->to($viewBooks);
+
+        Bouncer::allow($staff)->to($manageBooks);
+        Bouncer::allow($staff)->to($viewBooks);
+
+        Bouncer::allow($member)->to($viewBooks);
+
+
+        // Assign role to users
+        $user = User::where('email', 'admin@mylib.info')->first();
+        Bouncer::assign($admin)->to($user);
+
+        $user = User::where('email', 'user1@mylib.info')->first();
+        Bouncer::assign($staff)->to($user);
+
+        $user = User::where('email', 'user2@mylib.info')->first();
+        Bouncer::assign($member)->to($user);
+
+    }
+}
--- app/User.php
+++ app/User.php
@@ -5,11 +5,13 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Silber\Bouncer\Database\HasRolesAndAbilities;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
+    use HasRolesAndAbilities;
 
     /**
      * The attributes that are mass assignable.
